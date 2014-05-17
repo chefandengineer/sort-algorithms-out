4902f791c77c326ecad52f63cbac0748bcaa2457
chefandengineer <474667070@qq.com>
2014-05-17 00:38:55 -0400

    bst

--- src/cracking/treesgraphs/BalancedBinaryTreeChecker.java
+++ src/cracking/treesgraphs/BalancedBinaryTreeChecker.java
@@ -5,7 +5,43 @@ package cracking.treesgraphs;
  *
  */
 public class BalancedBinaryTreeChecker {
+	public static boolean check(Node node){
+		if (node == null){
+			return false;
+		}
+		return (max(node) - min(node)) <= 1;
+	}
 	
+	public static int max(Node node) {
+		if (node == null){
+			return 0;
+		}
+		return Math.max(max(node.left), max(node.right)) + 1;
+	}
+	
+	public static int min(Node node) {
+		if (node == null){
+			return 0;
+		}
+		return Math.min(min(node.left), min(node.right)) + 1;
+	}
+	
+	public static void main(String[] args){
+		CommonTree tree = new CommonTree();
+		tree.makeBalanceTree();
+		Node root = tree.getRoot();
+		System.out.println(max(root));
+		System.out.println(min(root));
+		
+		System.out.println(check(root));
+		
+		tree.makeUnBalanceTree();
+		root = tree.getRoot();
+		System.out.println(max(root));
+		System.out.println(min(root));
+		System.out.println(check(root));
+
+	}
 }
 
 
